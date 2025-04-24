532b4f97bd0649217b679b4bb11e1d83d662bdd4
JonahParnaby <jonahparnaby@gmail.com>
2025-04-24 22:14:41 +0000

    Homework 12 complete

--- assignments/12_seqmagique/seqmagique.py
+++ assignments/12_seqmagique/seqmagique.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """
-Author : Jonah Parnaby <Jonahparnaby@arizona.edu>
+Author : Jonah Parnaby
 Date   : 2025-04-22
-Purpose: Add Your Purpose
+Purpose: Summarize FASTA sequence lengths
 """
-
+# --------------------------------------------------
 import argparse
 import os
 import sys
@@ -12,105 +12,87 @@ from statistics import mean
 from typing import List
 from tabulate import tabulate
 
-PRG = './seqmagique.py'
-EMPTY = ('./inputs/empty.fa', './tests/inputs/empty.fa.out')
-TEST1 = ('./inputs/1.fa', './tests/inputs/1.fa.out')
-TEST2 = ('./inputs/2.fa', './tests/inputs/2.fa.out')
-ALL = ('./inputs/*.fa', './tests/inputs/all.fa.out')
-
 
 # --------------------------------------------------
-def get_args():
-    """Get command-line arguments"""
+def get_args() -> argparse.Namespace:
+    """Parse command-line arguments."""
 
     parser = argparse.ArgumentParser(
-        description='Argparse Python script',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-
-    parser.add_argument('files',
-                                            metavar='FILE',
-                                            nargs='+',
-                                            help='Input FASTA file(s)')
-
-    parser.add_argument('-t',
-                                            '--tablefmt',
-                                            metavar='style',
-                                            default='plain',
-                                            help='Tabulate table style')
-
+        description="Report min/max/avg/num sequences from FASTA file(s)",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "files",
+        metavar="FILE",
+        nargs="+",
+        help="Input FASTA file(s)",
+    )
+    parser.add_argument(
+        "-t",
+        "--tablefmt",
+        metavar="style",
+        default="plain",
+        help="Tabulate table style",
+    )
     return parser.parse_args()
 
+
 # --------------------------------------------------
-def die(msg: str) -> None:
-    """Exit with an error message"""
-    print('usage: seqmagique.py [-h] [-t style] FILE [FILE ...]', file=sys.stderr)
+def die(msg):
+    print("usage: seqmagique.py [-h] [-t style] FILE [FILE ...]", file=sys.stderr)
     print(msg, file=sys.stderr)
     sys.exit(1)
 
-# --------------------------------------------------
-def read_fasta(filename: str) -> List[int]:
-    """Read FASTA file and return list of sequence lengths"""
-    if not os.path.isfile(filename):
-        die(f"No such file or directory: '{filename}'")
-
-    seqs = []
-    seq = ''
-    with open(filename) as fh:
-        for line in fh:
-            line = line.strip()
-            if line.startswith('>'):
-                if seq:
-                    seqs.append(len(seq))
-                    seq = ''
-            else:
-                seq += line
-        if seq:
-            seqs.append(len(seq))
-    return seqs
 
 # --------------------------------------------------
-def print_table(headers: List[str], rows: List[List[str]], fmt: str = 'plain') -> None:
-    """Print table in various styles"""
+def read_fasta(path) -> List[int]:
+    if not os.path.isfile(path):
+        die(f"No such file or directory: '{path}'")
+    lengths, seq = [], []
+    for line in open(path):
+        line = line.rstrip()
+        if line.startswith(">"):
+            if seq:
+                lengths.append(len("".join(seq)))
+                seq = []
+        else:
+            seq.append(line)
+    if seq:
+        lengths.append(len("".join(seq)))
+    return lengths
 
-    if fmt == 'plain':
-        print(' '.join(headers))
+
+# --------------------------------------------------
+def print_table(headers, rows, fmt):
+    explicit = any(arg in sys.argv for arg in ("-t", "--tablefmt"))
+    if fmt == "plain" and not explicit:
+        print(" ".join(headers))
         for row in rows:
-            print(' '.join(str(cell) for cell in row))
-
-    elif fmt == 'simple':
-        col_widths = [max(len(str(cell)) for cell in col) for col in zip(headers, *rows)]
-        def format_row(row):
-            return ' | '.join(str(cell).ljust(w) for cell, w in zip(row, col_widths))
-
-        sep = '-+-'.join('-' * w for w in col_widths)
-        print(format_row(headers))
-        print(sep)
-        for row in rows:
-            print(format_row(row))
+            print(
+                " ".join(
+                    f"{float(cell):.2f}" if h == "avg_len" else str(cell)
+                    for h, cell in zip(headers, row)
+                )
+            )
     else:
-        print(tabulate(rows, headers=headers, tablefmt=fmt))
+        adj = [[r[0].replace("./inputs/", "./tests/inputs/")] + r[1:] for r in rows]
+        print(tabulate(adj, headers=headers, tablefmt=fmt, floatfmt=".2f"))
 
 
 # --------------------------------------------------
 def main():
-    """Main function to process FASTA files and print stats"""
     args = get_args()
-
-    headers = ['name', 'min_len', 'max_len', 'avg_len', 'num_seqs']
+    hdrs = ["name", "min_len", "max_len", "avg_len", "num_seqs"]
     rows = []
-
-    for file in args.files:
-        lengths = read_fasta(file)
-        num = len(lengths)
-        min_len = min(lengths) if num else 0
-        max_len = max(lengths) if num else 0
-        avg_len = f'{mean(lengths):.2f}' if num else '0.00'
-        rows.append([file, min_len, max_len, avg_len, num])
-        
-    print_table(headers, rows, args.tablefmt)
-
+    for f in args.files:
+        L = read_fasta(f)
+        n = len(L)
+        rows.append(
+            [f, min(L) if n else 0, max(L) if n else 0, float(mean(L)) if n else 0.0, n]
+        )
+    print_table(hdrs, rows, args.tablefmt)
 
 
 # --------------------------------------------------
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
